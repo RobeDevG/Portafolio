dc01b7572035670da0d5599e3df1a54219332f8d
Roberto <robertodev97@gmail.com>
2023-10-20 15:50:25 -0400

    slaiderMovile

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menuCheck = document.getElementById('menu')
 const menu = document.querySelector('.nav__menu-links')
-
+const cardsWindows = document.querySelector('.card__container')
+const cardsMovile = document.querySelector('.slaider__container')
 
 menuCheck.addEventListener('change', mostrarMenu)
 
