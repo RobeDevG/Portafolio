ee2412d49fe4b09e7b682543b44f7a65e970eeb5
Roberto <robertodev97@gmail.com>
2023-10-22 19:46:33 -0400

    somehange

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const menuCheck = document.getElementById('menu')
 const menu = document.querySelector('.nav__menu-links')
-
+const activeMenu = document.querySelectorAll('.nav__menu-link')
 
 menuCheck.addEventListener('change', mostrarMenu)
 
 function mostrarMenu() {
     menu.classList.toggle('hidden')
 }
+
+activeMenu.forEach((e , i) => { 
+    activeMenu[i].addEventListener('click', mostrarMenu)
+})
