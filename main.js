e2ac0cd6b37afa2102776b1b2d11a084a56337e0
Roberto <robertodev97@gmail.com>
2023-10-22 14:01:55 -0400

    some change

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const menuCheck = document.getElementById('menu')
 const menu = document.querySelector('.nav__menu-links')
-const cardsWindows = document.querySelector('.card__container')
-const cardsMovile = document.querySelector('.slaider__container')
+
 
 menuCheck.addEventListener('change', mostrarMenu)
 
-function mostrarMenu(){
+function mostrarMenu() {
     menu.classList.toggle('hidden')
 }
